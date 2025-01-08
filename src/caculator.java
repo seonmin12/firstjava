6ba9597ffed36ac6b12bbe5e5db8d57e18c2dd84
seonmin12 <seonmink12@naver.com>
2025-01-08 16:33:26 +0900

    "first commit"

---
+++ src/caculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class caculator {
+    public static void main(String[] args) {
+        //1. 데이터 자유 입력
+        Scanner in = new Scanner(System.in);
+        System.out.println("first number input: ");
+        int a = in.nextInt(); //1
+        System.out.println("end number input: ");
+        int b = in.nextInt(); //100
+        int sum = 0;
+        //2. 데이터 처리
+        sum = a + b;
+
+        for (int i = a; i <= b; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+            if (i % 2 == 1) {
+                sum = sum + i; //sum += i; }
+            }
+        }
+
+            //문제: for문을 이용하여 1-100까지의 합을 구하는 결과 출력
+
+        //3. 데이터 결과 저장
+        //4. 결과 출력
+        System.out.println("결과값 => " + a +"+" + b + "=" + sum);
+
+
+
+    }
+}
+
+
+
---
+++ src/ex.java
@@ -0,0 +1,7 @@
+public class ex {
+    public static void main(String[] args) {
+        System.out.println("첫 번째 프로그램입니다.");
+        System.out.println("두 번째 프로그램입니다. 한 줄에 여러 문장을 출력합니다.");
+        System.out.println("my name is Hong Gil Dong. \nI am 13 years old");
+    }
+}
